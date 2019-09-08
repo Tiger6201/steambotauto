5fd35f86d472df90c81a8741f82d1dfdd7495e1a
dilawfird <tiger_trading@outlook.fr>
2019-09-08 23:18:04 +0200

    Clean the code/files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import '../App.css';
 import { BrowserRouter, Route } from "react-router-dom";
-import Home from '../pages/home';
-import Pricing from '../pages/pricing';
-import About from '../pages/about';
 import Panel from '../pages/panel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,12 +8,7 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/pricing" component={Pricing} />
-      <Route path="/login" component={Home} />
-      <Route path="/panel" component={Panel} />
-      <Route path="/home" component={Home} />
+      <Route path="/" exact component={Panel} />
     </BrowserRouter>
   );
 }
--- client/src/containers/panel-header.js
+++ client/src/containers/panel-header.js
@@ -5,14 +5,14 @@ function PanelHeader() {
     return (
 
 <nav className="navbar navbar-expand navbar-dark bg-dark">
-  <h1 className="" href="#">SteamIdle</h1>
+  <h1 className="" href="#">Steambotauto</h1>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarCollapse">
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
-        <button className="nav-link" style={{display:"contents"}}><Link className="p-2 text-light" to="/home">Home</Link><span className="sr-only">(current)</span></button>
+        <button className="nav-link" style={{display:"contents"}}><Link className="p-2 text-light" to="/">Home</Link></button>
       </li>
     </ul>
   </div>
