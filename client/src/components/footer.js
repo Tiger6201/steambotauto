b033a2ceab1594f03da31faf3b6ddb909d232f48
Choudhary Muhammad Osama <chmosama@users.noreply.github.com>
2022-01-17 22:03:36 +0500

    Update footer.js

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
 <footer className="footer">
       <div className="container text-center">
-        <span style={{color:"grey"}}>By <button style={{display: "contents", color:'white'}}>@Dilawfird</button></span>
+        <span style={{color:"grey"}}>By <button style={{display: "contents", color:'white'}}>PK_Sec#4333</button></span>
         
       </div>
     </footer>
