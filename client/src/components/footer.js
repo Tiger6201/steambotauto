4258713e7b393fcd14a2e4aac2b0a8207cb2fa29
Choudhary Muhammad Osama <chmosama@users.noreply.github.com>
2022-07-31 21:57:56 +0500

    Update footer.js

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,6 +3,11 @@ import React from 'react';
 
 function Footer() {
     return (
+    <footer className="footer">
+      <div className="container text-center">
+        <span style={{color:"grey"}}>By <button style={{display: "contents", color:'white'}}>PK_Sec#4333</button></span>
+      </div>
+    </footer>
 
     )
 }
