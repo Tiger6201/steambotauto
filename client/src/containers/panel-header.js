e7df944d038a868b4c5f545494a519b18fad5bc0
Choudhary Muhammad Osama <chmosama@users.noreply.github.com>
2022-07-31 21:52:46 +0500

    Update panel-header.js

--- client/src/containers/panel-header.js
+++ client/src/containers/panel-header.js
@@ -5,7 +5,8 @@ function PanelHeader() {
     return (
 
 <nav className="navbar navbar-expand navbar-dark bg-dark">
-  <h1 className="" href="#">Steambotauto</h1>
+  <h1 className="" href="#">Hour Booster    </h1>
+  <span style={{color:"grey"}}>By <button style={{display: "contents", color:'white'}}>PK_Sec#4333</button></span>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
