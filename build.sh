ceeaa5502966e26686233b07d197d1442d08fdc0
Choudhary Muhammad Osama <chmosama@users.noreply.github.com>
2023-09-26 15:01:28 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,29 @@
 # !/bin/bash
 if [ "$CF_PAGES_BRANCH" == "production" ]; then
+  echo "Command 1"
   npm install express
+  echo "Command 2"
   npm install --prefix client
+  echo "Command 3"
   npm run build --prefix client
+  echo "Command 4"
   node index.js
-
 elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
+  echo "Command 1"
   npm install express
+  echo "Command 2"
   npm install --prefix client
+  echo "Command 3"
   npm run build --prefix client
+  echo "Command 4"
   node index.js
 else
+  echo "Command 1"
   npm install express
+  echo "Command 2"
   npm install --prefix client
+  echo "Command 3"
   npm run build --prefix client
+  echo "Command 4"
   node index.js
 fi
